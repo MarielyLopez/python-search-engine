9671dbff5f01bffcef343c7beb91bf86496987d7
MarielyLopez <mariielylopez795@gmail.com>
2015-11-16 17:46:08 -0600

    Now you can enter pages and can enter the word to search

--- application.py
+++ application.py
@@ -16,17 +16,63 @@ def insert_url():
             apply_for_url = urllib2.urlopen(str(comp_page))
             reading = apply_for_url.read()
             apply_for_url.close()
-            print "      *Greate!"
+            print "      Greate!"
             break
         except:
-            print "   *Page not found!"
+            print "   Page not found! :("
 
 
 def enter_of_answers():
     answer_one = insert_url()
     answer_two = insert_url()
-    print "The answer is:"
-    return answer_one, answer_two
+    insert_word()
 
 
-insert_url()
+def insert_word(reading):
+    print ""
+    word = raw_input("What word you want to find? ")
+    answer_word = str(word).isspace()
+    emptynumb = len(word)
+    if answer_word == False and emptynumb > 0:
+        searcher = re.findall(word, reading)
+        coincidence = len(searcher)
+    else:
+        print "Enter some data"
+
+def verific_num_words():
+
+
+
+
+        if coincidence > coincidencetwo:
+            print ""
+            print "Words found on", str(tofind), ":", str(coincidence)
+            print "Words found on", str(tofindtwo), ":", str(coincidencetwo)
+            print ""
+            print "  >>*We recommend", str(tofind), "because it has more matches."
+            break
+        elif coincidencetwo > coincidence:
+            print ""
+            print "Words found on", str(tofind), ":", str(coincidence)
+            print "Words found on", str(tofindtwo), ":", str(coincidencetwo)
+            print ""
+            print "We recommend", str(tofindtwo), "because it has more matches."
+            break
+        elif coincidence == coincidencetwo:
+            print ""
+            print "Words found on", str(tofind), ":", str(coincidence)
+            print "Words found on", str(tofindtwo), ":", str(coincidencetwo)
+            print ""
+            print "You can visit any website, because the results are the same."
+            break
+        else:
+            print ""
+            print "No matches found."
+            break
+    else:
+        print "Enter some data"
+
+
+
+#insert_url()
+enter_of_answers()
