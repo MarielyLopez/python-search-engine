5e3a63c9255e9597509602f315871e5145f47ee6
MarielyLopez <mariielylopez795@gmail.com>
2015-11-19 18:26:24 -0600

    it has become functional,verification data,pending.

--- application.py
+++ application.py
@@ -1,10 +1,6 @@
-# Aqui escribe tu codigo
-
-print "Hello World"
-
+"""Search engine"""
 import urllib2
 import re
-
 
 def insert_url():
     print ""
@@ -16,63 +12,47 @@ def insert_url():
             apply_for_url = urllib2.urlopen(str(comp_page))
             reading = apply_for_url.read()
             apply_for_url.close()
-            print "      Greate!"
+            print "      Greate! XD "
+            return comp_page, reading
             break
         except:
             print "   Page not found! :("
 
 
 def enter_of_answers():
-    answer_one = insert_url()
-    answer_two = insert_url()
-    insert_word()
+    link1, answer_one = insert_url()
+    link2, answer_two = insert_url()
+    insert_word(answer_one,answer_two,link1,link2)
 
-
-def insert_word(reading):
+def insert_word(answer_one,answer_two,link1,link2):
     print ""
-    word = raw_input("What word you want to find? ")
-    answer_word = str(word).isspace()
-    emptynumb = len(word)
-    if answer_word == False and emptynumb > 0:
-        searcher = re.findall(word, reading)
-        coincidence = len(searcher)
-    else:
-        print "Enter some data"
-
-def verific_num_words():
-
-
-
-
-        if coincidence > coincidencetwo:
-            print ""
-            print "Words found on", str(tofind), ":", str(coincidence)
-            print "Words found on", str(tofindtwo), ":", str(coincidencetwo)
-            print ""
-            print "  >>*We recommend", str(tofind), "because it has more matches."
-            break
-        elif coincidencetwo > coincidence:
-            print ""
-            print "Words found on", str(tofind), ":", str(coincidence)
-            print "Words found on", str(tofindtwo), ":", str(coincidencetwo)
-            print ""
-            print "We recommend", str(tofindtwo), "because it has more matches."
-            break
-        elif coincidence == coincidencetwo:
-            print ""
-            print "Words found on", str(tofind), ":", str(coincidence)
-            print "Words found on", str(tofindtwo), ":", str(coincidencetwo)
-            print ""
-            print "You can visit any website, because the results are the same."
+    words = True
+    while words == True:
+        word = raw_input("What word you want to find? ")
+        answer_word = str(word).isspace()
+        emptynumb = len(word)
+        if answer_word == False and emptynumb > 0:
+            searcher1 = re.findall(word, answer_one)
+            searcher2 = re.findall(word, answer_two)
+            coincidence1 = len(searcher1)
+            coincidence2 = len(searcher2)
+            print " On page to ",link1 +"it was found: ",coincidence1,"words."
+            print " On page to ",link2 +"it was found: ",coincidence2,"words."
+            print " "
+            print_recomending(coincidence1,coincidence2,link1,link2)
             break
         else:
-            print ""
-            print "No matches found."
-            break
+            print "Enter some data"
+
+def print_recomending(coincidence1,coincidence2,link1,link2):
+    if coincidence1 > coincidence2:
+        print"  We recommend",link1,"because it has more matches."
+    elif coincidence2 > coincidence1:
+        print"  We recommend",link2,"because it has more matches."
+    elif coincidence1 == coincidence2:
+        print "You can visit any website, because the results are the same."
     else:
-        print "Enter some data"
+        "No matches found."
 
 
-
-#insert_url()
 enter_of_answers()
